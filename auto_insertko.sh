9eb5ce6f993b8aa6fa765a576691befae0a2ff7a
lichuang18 <983459843@qq.com>
2026-09-24 16:11:48 +0800

    only_c0c1

--- auto_insertko.sh
+++ auto_insertko.sh
@@ -3,6 +3,6 @@ umount ./test_mount
 modprobe f2fs
 rmmod cf2fs.ko
 insmod cf2fs.ko
-mkfs.f2fs -f /dev/nvme1n1
-mount -t cf2fs  -o mode=lfs /dev/nvme1n1 ./test_mount 
+sudo mkfs.f2fs -f -O extra_attr,compression /dev/nvme1n1
+mount -t cf2fs  -o mode=lfs,compress_algorithm=lz4,compress_log_size=2,compress_mode=fs,compress_extension='*' /dev/nvme1n1 ./test_mount 
 # mount -t ef2fs  -o mode=lfs /dev/nvme2n1 /mnt
